923c35ad5b97898251150ab35f600501ca75d5f9
Eric Citaire <ecitaire@mediametrie.fr>
2016-05-10 12:56:16 +0200

    Add boto3 and boto3_wait modules

---
+++ cloud/amazon/boto3.py
@@ -0,0 +1,205 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# (c) 2016, René Moser <mail@renemoser.net>
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible. If not, see <http://www.gnu.org/licenses/>.
+
+DOCUMENTATION = '''
+---
+module: boto3 
+short_description: Make AWS API calls using boto3 client.
+description:
+    - This is a generic module to make AWS API calls using boto3 client instead of using the CLI.
+    - This module is not idempontent! The idempotency needs to be implemented in the playbook by making additional API calls or reading the return value. This module will always yield a changed state.
+version_added: 2.2
+requirements:
+    - boto3
+options:
+  service:
+    description:
+      - The name of the service (ie. 's3', 'ec2', 'rds', 'emr', etc.)
+    required: true
+    default: null 
+    aliases: ['name']
+  operation:
+    description:
+      - Service operation to call. Maps to client methods of the same name. See boto3 documentation.
+    required: true
+    default: null
+    aliases: []
+  parameters:
+    description:
+      - A hash/dictionary representing operations parameters. See boto3 documentation.
+    required: false
+    default: null
+    aliases: []
+  region:
+    description:
+      - AWS region.
+    required: false
+    default: present
+    aliases: []
+  aws_access_key:
+    description:
+      - AWS access key.
+    required: false
+    default: null
+    aliases: ['ec2_access_key', 'access_key']
+  aws_secret_key:
+    description:
+      - AWS secret key.
+    required: false
+    default: null
+    aliases: ['ec2_secret_key', 'secret_key']
+author: "Eric Citaire"
+'''
+
+EXAMPLES = '''
+- name: Describe running EMR Clusters
+  boto3:
+    name: emr
+    region: us-east-1
+    operation: list_clusters
+    parameters:
+      ClusterStates:
+        - STARTING
+        - BOOTSTRAPPING
+        - RUNNING
+        - WAITING
+      CreatedAfter: "2016-05-03 13:30:00"
+  register: emr
+
+# if 'my-jobflow' cluster does not exist, create it.
+- name: Create EMR Cluster
+  boto3:
+    name: emr
+    region: us-east-1
+    operation: run_job_flow
+    parameters:
+      Name: my-jobflow
+      Applications:
+        - Name: Hive
+      Instances:
+        Ec2KeyName: my-key
+        Ec2SubnetId: subnet-xxxxxxx
+        EmrManagedSlaveSecurityGroup: sg-xxxxxxx
+        EmrManagedMasterSecurityGroup: sg-xxxxxxx
+        InstanceGroups:
+          - InstanceCount: 1
+            InstanceRole: MASTER
+            InstanceType: m1.medium
+            Market: ON_DEMAND
+          - InstanceCount: 1
+            InstanceRole: CORE
+            InstanceType: m1.medium
+            Name: Core Instance Group
+            Market: ON_DEMAND
+      JobFlowRole: EMR_EC2_DefaultRole
+      ServiceRole: EMR_DefaultRole
+      ReleaseLabel: emr-4.6.0
+      Steps:
+        - HadoopJarStep:
+            Args:
+              - state-pusher-script
+            Jar: command-runner.jar
+          ActionOnFailure: TERMINATE_CLUSTER
+          Name: Setup Hadoop Debugging
+     VisibleToAllUsers: True
+  when: "'my-jobflow' not in (emr.response.Clusters | map(attribute='Name'))"
+'''
+
+RETURN = '''
+response:
+    description: boto3 response, depending on the operation called. See boto3 documentation.
+    returned: success
+    type: dict
+    sample: { "Clusters": [ ], "ResponseMetadata": { "HTTPStatusCode": 200, "RequestId": "xxxxxxxx-xxxx-xxxx-xxxx-xxx"xxxxxxxx" } }
+'''
+
+try:
+  import boto3
+  from botocore.exceptions import *
+  HAS_BOTO3 = True
+except ImportError:
+  HAS_BOTO3 = False
+
+def get_operation_method(client, operation_name):
+  valid = True
+  # private method
+  if operation_name[0] == '_':
+    valid = False
+  # client utility methods
+  utility_methods = [
+      'can_paginate',
+      'generate_presigned_url',
+      'get_paginator',
+      'get_waiter',
+      'meta',
+      'waiter_names'
+    ]
+  if operation_name in utility_methods:
+    valid = False
+  # not an attribute
+  if operation_name not in dir(client):
+    valid = False
+  # not callable
+  operation_method = getattr(client, operation_name, None) if valid else None
+  if not callable(operation_method):
+    valid = False
+  if valid:
+    return operation_method
+  else:
+    return None
+
+def main():
+  module = AnsibleModule(
+    argument_spec = dict(
+      service=dict(required=True, type='str', aliases=['name']),
+      operation=dict(required=True, type='str'),
+      parameters=dict(default={}, type='dict'),
+      region=dict(type='str'),
+      aws_access_key=dict(type='str', aliases=['ec2_access_key', 'access_key']),
+      aws_secret_key=dict(type='str', aliases=['ec2_secret_key', 'secret_key'])
+    ),
+    supports_check_mode=False
+  )
+
+  if not HAS_BOTO3:
+    module.fail_json(msg='boto3 is required for this module')
+
+  service = module.params.get('service')
+  operation = module.params.get('operation')
+  parameters = module.params.get('parameters')
+  region = module.params.get('region')
+  aws_access_key = module.params.get('aws_access_key')
+  aws_secret_key = module.params.get('aws_secret_key')
+
+  try:
+    client = boto3.client(service, region_name=region, aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
+    operation_method = get_operation_method(client, operation)
+    if not operation_method:
+      module.fail_json(msg='"{}" is not a valid operation for service "{}".'.format(operation, name))
+    response = operation_method(**parameters)
+  except Exception as e:
+    module.fail_json(msg=e.message)
+  
+  module.exit_json(changed=True, response=response)
+
+# import module snippets
+from ansible.module_utils.basic import *
+if __name__ == '__main__':
+  main()
